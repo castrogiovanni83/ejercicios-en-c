d9aeee8c24b6004eaffcc200baa8ea0044576ec5
Mauro Castrogiovanni <68161442+castrogiovanni83@users.noreply.github.com>
2023-04-02 18:33:43 -0300

    Add files via upload

---
+++ practica lab 1,ejercicio.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+int numero1;
+int numero2;
+int resultado;
+
+cout<< " ingrese el primer numero"<< endl;
+cin>> numero1 ;
+cout<< "ingrese el segundo numero"<< endl;
+cin>> numero2 ;
+resultado  = numero1 + numero2 ;
+cout<<  "el resultado es : " << endl << resultado;
+
+
+
+return 0;
+}
