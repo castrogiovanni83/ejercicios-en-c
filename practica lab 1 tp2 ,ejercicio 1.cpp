1de864e99060e85e98824eacd58b653f52d0bc15
Mauro Castrogiovanni <68161442+castrogiovanni83@users.noreply.github.com>
2023-04-02 18:34:17 -0300

    Add files via upload

---
+++ practica lab 1 tp2 ,ejercicio 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int numero;
+
+cout<< "ingrese el numero"<< endl;
+cin>> numero;
+if (numero > 0 ){
+
+cout<< " el numero es positivo";
+}
+else if(numero == 0){
+                cout<< " el numero es cero";
+
+}
+    else{
+            cout<< " el numero es negativo";
+}
+
+
+
+return 0;
+}
